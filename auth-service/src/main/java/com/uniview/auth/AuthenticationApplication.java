90a2941cbfc71f13f7ca36605e26de9e6f16afa0
Kurt LEE <59387051+kurtleee@users.noreply.github.com>
2024-08-27 16:29:44 +0800

    Rename AuthApp @kurtleee

---
+++ auth-service/src/main/java/com/uniview/auth/AuthenticationApplication.java
@@ -0,0 +1,15 @@
+package com.uniview.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kurt LEE
+ * @created 2024/08/27 - 下午4:02
+ */
+@SpringBootApplication
+public class AuthenticationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationApplication.class, args);
+    }
+}
