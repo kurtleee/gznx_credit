9ff4196be2aa353d5348509a5b8e176256138fde
Kurt LEE <59387051+kurtleee@users.noreply.github.com>
2024-08-28 14:34:34 +0800

    贷款管理管理模块实体类/Mapper创建

---
+++ loan-service/src/main/java/com/uniview/loan/service/RepaymentPlanService.java
@@ -0,0 +1,13 @@
+package com.uniview.loan.service;
+
+import com.uniview.loan.pojo.RepaymentPlan;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author kurtlee
+* @description 针对表【repayment_plan】的数据库操作Service
+* @createDate 2024-08-28 14:33:32
+*/
+public interface RepaymentPlanService extends IService<RepaymentPlan> {
+
+}
---
+++ loan-service/src/main/java/com/uniview/loan/service/impl/RepaymentPlanServiceImpl.java
@@ -0,0 +1,22 @@
+package com.uniview.loan.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.uniview.loan.pojo.RepaymentPlan;
+import com.uniview.loan.service.RepaymentPlanService;
+import com.uniview.loan.mapper.RepaymentPlanMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author kurtlee
+* @description 针对表【repayment_plan】的数据库操作Service实现
+* @createDate 2024-08-28 14:33:32
+*/
+@Service
+public class RepaymentPlanServiceImpl extends ServiceImpl<RepaymentPlanMapper, RepaymentPlan>
+    implements RepaymentPlanService{
+
+}
+
+
+
+
